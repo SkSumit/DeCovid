95bd2e32bf1141dad78038081dcda502ee04199f
SkSumit <kolsum24@gmail.com>
2020-05-29 19:31:56 +0530

    layout changes

--- index.js
+++ index.js
@@ -24,20 +24,54 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 var overallStatsNumber = document.querySelectorAll("p.subtitle");
-var worldStatsTable = document.querySelector("#worldStatsTable");
-var indiaStatsTable = document.querySelector("#indiaStatsTable");
 var table = document.querySelectorAll("table");
+var stateName = [],stateActiveList = [],stateConfirmedList = [],stateDeathsList = [],stateRecoveredList = [],totalDateTimeline = [], totalConfirmedTimeline = []
+var totalDeathTimeline = [],totalRecoveredTimeline = [],dailyConfirmedTimeline = [], dailyDeathTimeline = [],dailyRecoveredTimeline = [];
+var wtotalDeathTimeline = [],wtotalRecoveredTimeline = [],wdailyConfirmedTimeline = [], wdailyDeathTimeline = [],wdailyRecoveredTimeline = [],wtotalConfirmedTimeline = []
 
+var pievalC=[],pievalD=[],pievalR=[],wtotalDateTimeline =[]
 
 fetch("https://corona-api.com/timeline").then((response) => {
   response.json().then((data) => {
     if (data.error) {
       console.log(data.error);
     } else {
-     for(i=0 ; i<6 ; i++){
-      overallStatsNumber[i].textContent = data.data[0].confirmed;
-     }
-    }
+        
+        var worldTimeline = data.data
+        worldTimeline.forEach((timeline)=>{
+                 wtotalDateTimeline.push(timeline.date)
+                 wtotalConfirmedTimeline.push(timeline.confirmed)
+      wtotalDeathTimeline.push(timeline.deaths)
+      wtotalRecoveredTimeline.push(timeline.recovered)
+      wdailyConfirmedTimeline.push(timeline.new_confirmed)
+      wdailyDeathTimeline.push(timeline.new_deaths)
+      wdailyRecoveredTimeline.push(timeline.new_recovered)
+        })
+
+
+      
+
+      overallStatsNumber[0].textContent = data.data[0].confirmed;
+      overallStatsNumber[1].textContent = data.data[0].deaths;
+      overallStatsNumber[2].textContent = data.data[0].recovered;
+      overallStatsNumber[3].textContent = data.data[0].new_confirmed;
+      overallStatsNumber[4].textContent = data.data[0].new_deaths;
+      overallStatsNumber[5].textContent = data.data[0].new_recovered;
+    } 
+    wtotalDateTimeline.reverse()
+    wtotalConfirmedTimeline.reverse()
+    wtotalDeathTimeline.reverse()
+    wtotalRecoveredTimeline.reverse()
+    wdailyConfirmedTimeline.reverse()
+    wdailyDeathTimeline.reverse()
+    wdailyRecoveredTimeline.reverse()
+    chartW.update();
+    chartW1.update();
+    chartW2.update();
+    chartW3.update();
+    chartW4.update();
+    chartW5.update();
+   
   });
 });
 
@@ -47,13 +81,11 @@ fetch("https://corona-api.com/countries").then((response) => {
       console.log(data.error);
     } else {
       const countries = data.data;
-      // console.log(countries);
-
+     
       countries.forEach((country, index) => {
-        // console.log(country);
-        // console.log(country.latest_data);
-        // console.log(country.today);
-        const row = table[0].insertRow(index + 1);
+
+
+      const row = table[0].insertRow(index + 1);
         row.innerHTML = `<th>${country.name}</th> 
         <td>${country.latest_data.confirmed}</td>
         <td>${country.today.confirmed}</td>
@@ -62,34 +94,12 @@ fetch("https://corona-api.com/countries").then((response) => {
         <td>${country.latest_data.recovered}</td>
          <td>${country.latest_data.critical}</td>`;
       });
+      
     }
   });
 });
 
-function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-var stateName = [];
-var stateActiveList = [];
-var stateConfirmedList = [];
-var stateDeathsList = [];
-var stateRecoveredList = [];
-var stateColorList = [];
-var totalDateTimeline = [];
-var totalConfirmedTimeline = [];
-var totalDeathTimeline = [];
-var totalRecoveredTimeline = [];
-var dailyConfirmedTimeline = [];
-var dailyDeathTimeline = [];
-var dailyRecoveredTimeline = [];
-var pieConfirmed, pieDeaths, pieRecovered;
-
+//Options for ChartsJS
 var dataChartOptions = {
   legend: {
     display: false,
@@ -121,24 +131,23 @@ var dataChartOptions = {
     ],
   },
 };
-// var myChartTCC = document.getElementById("myChartTCC").getContext("2d");
+
 
 fetch("https://api.covid19india.org/data.json").then((response) => {
   response.json().then((data) => {
     var states = data.statewise;
     var totalTimeline = data.cases_time_series;
-
-    // console.log(pieConfirmed,pieDeaths,pieRecovered)
+    
     overallStatsNumber[6].textContent = data.statewise[0].confirmed;
     overallStatsNumber[7].textContent = data.statewise[0].deaths;
     overallStatsNumber[8].textContent = data.statewise[0].recovered;
     overallStatsNumber[9].textContent = data.statewise[0].deltaconfirmed;
     overallStatsNumber[10].textContent = data.statewise[0].deltadeaths;
     overallStatsNumber[11].textContent = data.statewise[0].deltarecovered;
-    checker = data.statewise[0].deltarecovered;
+    
 
     totalTimeline.forEach((timeline, index) => {
-      // console.log(timeline)
+     
       totalConfirmedTimeline.push(timeline.totalconfirmed);
       totalDateTimeline.push(timeline.date);
       totalDeathTimeline.push(timeline.totaldeceased);
@@ -154,10 +163,16 @@ fetch("https://api.covid19india.org/data.json").then((response) => {
       stateConfirmedList.push(state.confirmed);
       stateDeathsList.push(state.deaths);
       stateRecoveredList.push(state.recovered);
-      stateColorList.push(getRandomColor());
+      if(index==0){
+        pievalC.push(state.confirmed)
+        pievalD.push(state.deaths)
+        pievalR.push(state.recovered)
+        
+      }
+     
 
       const row = table[1].insertRow(index + 1);
-      row.innerHTML = `<th onclick="javascript:location.href='http://www.duolancers.codes'" >${state.state}</th> 
+      row.innerHTML = `<th onclick="javascript:location.href='/${stateName[index]}'" >${state.state}</th> 
       <td>${state.confirmed}</td>
       <td>${state.deltaconfirmed}</td>
       <td>${state.deaths}</td>
@@ -165,17 +180,13 @@ fetch("https://api.covid19india.org/data.json").then((response) => {
       <td>${state.recovered}</td>
        <td>${state.deltarecovered}</td>`;
     });
-    // console.log(data.cases_time_series);
-    pieConfirmed = data.statewise[0].confirmed;
-    pieDeaths = data.statewise[0].deaths;
-    pieRecovered = data.statewise[0].recovered;
 
     stateName.shift();
     stateConfirmedList.shift();
     stateActiveList.shift();
     stateDeathsList.shift();
     stateRecoveredList.shift();
-    // chartTCC.update();
+    
     chart1.update();
     chart.update();
     chart2.update();
@@ -183,22 +194,14 @@ fetch("https://api.covid19india.org/data.json").then((response) => {
     chart4.update();
     chart5.update();
     chart6.update();
-    pie.update();
-    // doughnut_chart.update()
+    pie.update()
+  
   })
 })
 
-// console.log(stateColorList)
-console.log(
-  pieConfirmed,
-  pieDeaths,
-  pieRecovered,
-  totalConfirmedTimeline,
-  // checker
-);
+
 var ctx = document.querySelectorAll(".myChart");
-// var ctx1 = document.querySelector("#myChart1");
-// console.log(ctx1)
+var ctxW = document.querySelectorAll(".myChartW");
 
 var chart = new Chart(ctx[0], {
   // The type of chart we want to create
@@ -210,9 +213,9 @@ var chart = new Chart(ctx[0], {
     datasets: [
       {
         label: "Confirmed Cases",
-        backgroundColor: "hsl(48, 100%, 67%)",
+        backgroundColor: "#007bff",
         // fill: false,
-        borderColor: "hsl(48, 100%, 67%)",
+        borderColor: "#007bff",
         data: totalConfirmedTimeline,
       },
     ],
@@ -221,6 +224,29 @@ var chart = new Chart(ctx[0], {
   // Configuration options go here
   options: dataChartOptions,
 });
+
+var chartW = new Chart(ctxW[0], {
+  // The type of chart we want to create
+  type: "line",
+
+  // The data for our dataset
+  data: {
+    labels: wtotalDateTimeline,
+    datasets: [
+      {
+        label: "Confirmed Cases",
+        backgroundColor: "#007bff",
+        // fill: false,
+        borderColor: "#007bff",
+        data: wtotalConfirmedTimeline,
+      },
+    ],
+  },
+
+  // Configuration options go here
+  options: dataChartOptions,
+});
+
 
 var chart1 = new Chart(ctx[1], {
   // The type of chart we want to create
@@ -232,10 +258,32 @@ var chart1 = new Chart(ctx[1], {
     datasets: [
       {
         label: "Death Cases",
-        backgroundColor: "hsl(348, 100%, 61%)",
+        backgroundColor: "#ff073a",
         // fill: false,
-        borderColor: "hsl(348, 100%, 61%)",
-        data: totalDeathTimeline,
+        borderColor: "#ff073a",
+        data:  wtotalConfirmedTimeline,
+      },
+    ],
+  },
+
+  // Configuration options go here
+  options: dataChartOptions,
+});
+
+var chartW1 = new Chart(ctxW[1], {
+  // The type of chart we want to create
+  type: "line",
+
+  // The data for our dataset
+  data: {
+    labels: wtotalDateTimeline,
+    datasets: [
+      {
+        label: "Death Cases",
+        backgroundColor: "#ff073a",
+        // fill: false,
+        borderColor: "#ff073a",
+        data: wtotalDeathTimeline,
       },
     ],
   },
@@ -254,9 +302,9 @@ var chart2 = new Chart(ctx[2], {
     datasets: [
       {
         label: "Recovered Cases",
-        backgroundColor: "hsl(141, 71%, 48%)",
+        backgroundColor: "#28a745",
         // fill: false,
-        borderColor: "hsl(141, 71%, 48%)",
+        borderColor: "#28a745",
         data: totalRecoveredTimeline,
       },
     ],
@@ -265,6 +313,29 @@ var chart2 = new Chart(ctx[2], {
   // Configuration options go here
   options: dataChartOptions,
 });
+
+var chartW2 = new Chart(ctxW[2], {
+  // The type of chart we want to create
+  type: "line",
+
+  // The data for our dataset
+  data: {
+    labels: wtotalDateTimeline,
+    datasets: [
+      {
+        label: "Recovered Cases",
+        backgroundColor: "#28a745",
+        // fill: false,
+        borderColor: "#28a745",
+        data: wtotalRecoveredTimeline,
+      },
+    ],
+  },
+
+  // Configuration options go here
+  options: dataChartOptions,
+});
+
 
 var chart3 = new Chart(ctx[3], {
   // The type of chart we want to create
@@ -276,10 +347,32 @@ var chart3 = new Chart(ctx[3], {
     datasets: [
       {
         label: "Confirmed Cases",
-        backgroundColor: "hsl(48, 100%, 67%)",
+        backgroundColor: "#007bff",
         // fill: false,
-        borderColor: "hsl(48, 100%, 67%)",
+        borderColor: "#007bff",
         data: dailyConfirmedTimeline,
+      },
+    ],
+  },
+
+  // Configuration options go here
+  options: dataChartOptions,
+});
+
+var chartW3 = new Chart(ctxW[3], {
+  // The type of chart we want to create
+  type: "line",
+
+  // The data for our dataset
+  data: {
+    labels: wtotalDateTimeline,
+    datasets: [
+      {
+        label: "Confirmed Cases",
+        backgroundColor: "#007bff",
+        // fill: false,
+        borderColor: "#007bff",
+        data: wdailyConfirmedTimeline,
       },
     ],
   },
@@ -298,10 +391,33 @@ var chart4 = new Chart(ctx[4], {
     datasets: [
       {
         label: "Death Cases",
-        backgroundColor: "hsl(348, 100%, 61%)",
+        backgroundColor: "#ff073a",
         // fill: false,
-        borderColor: "hsl(348, 100%, 61%)",
+        borderColor: "#ff073a",
         data: dailyDeathTimeline,
+      },
+    ],
+  },
+
+  // Configuration options go here
+  options: dataChartOptions,
+});
+
+
+var chartW4 = new Chart(ctxW[4], {
+  // The type of chart we want to create
+  type: "line",
+
+  // The data for our dataset
+  data: {
+    labels: wtotalDateTimeline,
+    datasets: [
+      {
+        label: "Death Cases",
+        backgroundColor: "#ff073a",
+        // fill: false,
+        borderColor: "#ff073a",
+        data: wdailyDeathTimeline,
       },
     ],
   },
@@ -320,10 +436,33 @@ var chart5 = new Chart(ctx[5], {
     datasets: [
       {
         label: "Recovered Cases",
-        backgroundColor: "hsl(141, 71%, 48%)",
+        backgroundColor: "#28a745",
         // fill: false,
-        borderColor: "hsl(141, 71%, 48%)",
+        borderColor: "#28a745",
         data: dailyRecoveredTimeline,
+      },
+    ],
+  },
+
+  // Configuration options go here
+  options: dataChartOptions,
+});
+
+
+var chartW5 = new Chart(ctxW[5], {
+  // The type of chart we want to create
+  type: "line",
+
+  // The data for our dataset
+  data: {
+    labels: wtotalDateTimeline,
+    datasets: [
+      {
+        label: "Recovered Cases",
+        backgroundColor: "#28a745",
+        // fill: false,
+        borderColor: "#28a745",
+        data: wdailyRecoveredTimeline,
       },
     ],
   },
@@ -342,23 +481,23 @@ var chart6 = new Chart(ctx[6], {
     datasets: [
       {
         label: "Confirmed Cases",
-        backgroundColor: "hsl(48, 100%, 67%)",
+        backgroundColor: "#007bff",
         fill: false,
-        borderColor: "hsl(48, 100%, 67%)",
+        borderColor: "#007bff",
         data: stateConfirmedList,
       },
       {
         label: "Death Cases",
-        backgroundColor: "hsl(348, 100%, 61%)",
+        backgroundColor: "#ff073a",
         // fill: false,
-        borderColor: "hsl(348, 100%, 61%)",
+        borderColor: "#ff073a",
         data: stateDeathsList,
       },
       {
         label: "Recovered Cases",
-        backgroundColor: "hsl(141, 71%, 48%)",
+        backgroundColor: "#28a745",
         // fill: false,
-        borderColor: "hsl(141, 71%, 48%)",
+        borderColor: "#28a745",
         data: stateRecoveredList,
       },
     ],
@@ -368,29 +507,29 @@ var chart6 = new Chart(ctx[6], {
   options: dataChartOptions,
 });
 
-// var pie = document.getElementById("doughnut-chart")
+
 var pie = new Chart(document.getElementById("doughnut-chart"), {
   type: "doughnut",
   data: {
     labels: ["Confirmed", "Deaths", "Recovered"],
     datasets: [
       {
-        label: "Population (millions)",
+        label: "Cases",
         backgroundColor: [
-          "#3e95cd",
-          "#8e5ea2",
-          "#3cba9f",
-          "#e8c3b9",
-          "#c45850",
+          "#007bff",
+          "#ff073a",
+          "#28a745",
+          
         ],
-        data: [pieConfirmed, pieDeaths, pieRecovered],
+        data: [pievalC,pievalD,pievalR],
       },
     ],
   },
   options: {
     title: {
       display: true,
-      text: "Predicted world population (millions) in 2050",
+      text: "The division of total number of cases",
     },
+    responsive: true,
   },
 });
